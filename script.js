98188b4eed8ed1ae50c4072e94372ec9c225fbcf
nazi-maisuradze <146000732+nazi-maisuradze@users.noreply.github.com>
2024-01-15 20:34:18 +0400

    https://prod.liveshare.vsengsaas.visualstudio.com/join?C23AFD5D8B0A37ABF74129E526E5C1B28492

---
+++ script.js
@@ -0,0 +1,43 @@
+// taskk  1 
+
+// for (let i = 5; i<=100; i++) {
+//     console.log(i);
+// }
+
+//taskk 2
+
+// let array1= [1, 2, 4, -1, -3, 8, 7, 0, -5, 18, 12];
+
+// for (let i=0; i<array1.length; i++){
+//     if (array1[i] > 0 && array1[i] <10){
+//         console.log(array1[i]);
+//     }
+// }
+
+// (ახალი ვარიანტი)
+
+// for (let i of array1){
+//     if (i>0 && i<10){
+//         console.log(i);
+//     }
+// }
+
+//task  3 
+// let array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (let i=0; i<array2.length; i++){
+//     if (array2=5){
+//         console.log("aris")
+//     }
+// }
+
+//taskk 4 
+
+let array3= [1, 2, 3, 4, 5];
+
+for(let i=0; i=array3.length; i++){
+    if 
+}
+
+
+//task 5 
+
